2a4e17580bae2d281c447909b39f678647446afd
kvetinac97 <kvetinac97@users.noreply.github.com>
2018-02-18 21:07:28 +0100

    [EXPERIMENTAL] Multiversion (#143)
    * Multiversion for NukkitX
    
    * Oh, I didn't see that
    
    * Warning - EXPERIMENTAL

--- src/main/java/cn/nukkit/network/protocol/BatchPacket.java
+++ src/main/java/cn/nukkit/network/protocol/BatchPacket.java
@@ -7,22 +7,21 @@ import cn.nukkit.network.CacheEncapsulatedPacket;
  * Nukkit Project
  */
 public class BatchPacket extends DataPacket {
-    public static final byte NETWORK_ID = ProtocolInfo.BATCH_PACKET;
 
     public byte[] payload;
 
     @Override
-    public byte pid() {
-        return NETWORK_ID;
+    public byte pid(PlayerProtocol protocol) {
+        return protocol.getPacketId("BATCH_PACKET");
     }
 
     @Override
-    public void decode() {
+    public void decode(PlayerProtocol protocol) {
         this.payload = this.get();
     }
 
     @Override
-    public void encode() {
+    public void encode(PlayerProtocol protocol) {
 
     }
 
@@ -36,4 +35,5 @@ public class BatchPacket extends DataPacket {
             }
         }
     }
+
 }
