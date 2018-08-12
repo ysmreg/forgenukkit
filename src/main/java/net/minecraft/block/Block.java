eb64c62c8ae67c67353af20f01a367a846631408
ysmreg <33682244+ysmreg@users.noreply.github.com>
2018-08-12 16:23:40 +0900

    Add files via upload

---
+++ src/main/java/net/minecraft/block/Block.java
@@ -0,0 +1,16 @@
+package net.minecraft.block;
+import net.minecraft.creativetab.CreativeTabs;
+public class Block {
+	public String name="Unknown";
+	public String texture="";
+	public int id=0;
+public void setCreativeTab(int a) {
+	
+}
+public void setBlockName(String name) {
+	this.name=name;
+}
+public void setBlockTextureName(String texname) {
+	texture=texname;
+}
+}
